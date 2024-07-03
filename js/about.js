adda45841776827e3b8917814c2e23f5a201fef6
Wolframich <112698120+Wolframich@users.noreply.github.com>
2024-07-03 22:42:15 +0300

    commit0

--- js/about.js
+++ js/about.js
@@ -1,26 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const text3Elements = document.querySelectorAll(".ongoingQuestinText3");
+// document.addEventListener("DOMContentLoaded", function() {
+//     const text3Elements = document.querySelectorAll(".ongoingQuestinText3");
 
-    text3Elements.forEach(function(text3) {
-      text3.addEventListener("click", function() {
-        const text4 = this.nextElementSibling;
+//     text3Elements.forEach(function(text3) {
+//       text3.addEventListener("click", function() {
+//         const text4 = this.nextElementSibling;
 
-        text4.classList.toggle("show");
+//         text4.classList.toggle("show");
 
-        text3.style.borderBottom = text4.classList.contains("show") ? "none" : "1px solid rgba(52, 56, 57, 1)";
-        text3.style.borderRadius = text3.classList.contains("show") ? "none" : "25px 25px 0px 0px";
-        text3.style.borderRadius = text4.classList.contains("show") ? "none" : "25px";
+//         text3.style.borderBottom = text4.classList.contains("show") ? "none" : "1px solid rgba(52, 56, 57, 1)";
+//         text3.style.borderRadius = text3.classList.contains("show") ? "none" : "25px 25px 0px 0px";
+//         text3.style.borderRadius = text4.classList.contains("show") ? "none" : "25px";
 
-        const image = this.querySelector("img");
+//         const image = this.querySelector("img");
 
-        if (text4.classList.contains("show")) {
-          image.src = "img/about/lineal/Vector 10.png";
-        } else {
-          image.src = "img/about/lineal/Vector 13.png";
-        }
-      });
-    });
-  });
+//         if (text4.classList.contains("show")) {
+//           image.src = "img/about/lineal/Vector 10.png";
+//         } else {
+//           image.src = "img/about/lineal/Vector 13.png";
+//         }
+//       });
+//     });
+//   });
 
 // function toggleText() {
 //     var answer = document.getElementById("answer");
