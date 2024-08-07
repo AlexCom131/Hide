cfd51fc9ed76c768f14fde32f48033502f3f2eb1
Wolframich <112698120+Wolframich@users.noreply.github.com>
2024-08-07 14:05:28 +0300

    commit11

--- blogService/database.js
+++ blogService/database.js
@@ -21,12 +21,12 @@ db.serialize(() => {
   )`);
 
   const stmt = db.prepare("INSERT INTO blogs (title, imagion, author, authorAvatar, date, content) VALUES (?,?, ?, ?, ?, ?)");
-  stmt.run("Blog Title 1", "/img/pictur/Blog Image.png", "Oliver Bennett1", "/img/logoClients/1Logo.png", "18 Jan 2022", "Lorem Ipsum Is a Dummy Text Used As The Heading Of a Blog");
-  stmt.run("Blog Title 2", "/img/pictur/Blog Image.png", "Щдуп Bennett2", "/img/logoClients/1Logo.png", "18 Jan 2022", "Another Dummy Text For Blog Heading");
-  stmt.run("Blog Title 3", "/img/pictur/Group 48095419.png", "Oliver іваів3", "/img/logoClients/1Logo.png", "18 Jan 2022", "Yet Another Dummy Text For Blog Heading");
-  stmt.run("Blog Title 4", "/img/pictur/Blog Image1.png", "Oliver іваів4", "/img/logoClients/1Logo.png", "18 Jan 2022", "Yet Another Dummy Text For Blog Heading");
-  stmt.run("Blog Title 5", "/img/pictur/Image.png", "Bogdan іваів5", "/img/logoClients/1Logo.png", "18 Jan 2022", "Yet Another Dummy Text For Blog Heading");
-  stmt.run("Blog Title 6", "/img/pictur/Group 48095419.png", "Bogdan іваів6", "/img/logoClients/1Logo.png", "18 Jan 2022", "Yet Another Dummy Text For Blog Heading");
+  stmt.run("Blog Title 1", "/img/pictur/Blog Image.png", "Oliver Bennett1", "/img/review/Ellipse 22.png", "18 Jan 2022", "Lorem Ipsum Is a Dummy Text Used As The Heading Of a Blog");
+  stmt.run("Blog Title 2", "/img/pictur/Blog Image.png", "Щдуп Bennett2", "/img/review/Ellipse 22.png", "18 Jan 2022", "Another Dummy Text For Blog Heading");
+  stmt.run("Blog Title 3", "/img/pictur/Group 48095419.png", "Oliver іваів3", "/img/review/Ellipse 22.png", "18 Jan 2022", "Yet Another Dummy Text For Blog Heading");
+  stmt.run("Blog Title 4", "/img/pictur/Blog Image1.png", "Oliver іваів4", "/img/review/Ellipse 22.png", "18 Jan 2022", "Yet Another Dummy Text For Blog Heading");
+  stmt.run("Blog Title 5", "/img/pictur/Image.png", "Bogdan іваів5", "/img/review/Ellipse 22.png", "18 Jan 2022", "Yet Another Dummy Text For Blog Heading");
+  stmt.run("Blog Title 6", "/img/pictur/Group 48095419.png", "Bogdan іваів6", "/img/review/Ellipse 22.png", "18 Jan 2022", "Yet Another Dummy Text For Blog Heading");
 
 
   stmt.finalize(() => {
